68b3f7c1261716b69adfb6f7f6c81cfb24f9ecb8
timelyportfolio <kent.russell@timelyportfolio.com>
2013-07-19 10:14:21 -0500

    first working version

--- code.R
+++ code.R
@@ -9,5 +9,47 @@
 #https://docs.google.com/a/timelyportfolio.com/spreadsheet/ccc?key=0AieeEIaS0AOsdHpPbmRaZkJtSW44M2pFQnRMaEJLcEE#gid=0
 
 releaseDates <- read.csv(
-  "https://docs.google.com/spreadsheet/pub?key=0AieeEIaS0AOsdHpPbmRaZkJtSW44M2pFQnRMaEJLcEE&output=csv"
+  "https://docs.google.com/spreadsheet/pub?key=0AieeEIaS0AOsdHpPbmRaZkJtSW44M2pFQnRMaEJLcEE&output=csv",
+  stringsAsFactors = FALSE
 )
+#delete last row since we will use R to fill timeline main
+releaseDates <- releaseDates[-nrow(releaseDates),]
+
+releaseList <- alply(releaseDates, 1, function(x){
+  list(
+    startDate = x$Start.Date,
+    headline = x$Headline,
+    text = getURLContent(x$Media, ssl.verifypeer = FALSE, useragent = "R"),
+    asset = list(media = x$Media)
+  )
+})
+
+
+# Create Timeline
+m = Timeline$new()
+m$main(
+  headline =  
+    paste0(
+      "R Release History ",
+      format(as.Date(tail(releaseDates,1)[,1],"%m/%d/%Y"),"%Y"), "-",
+      format(as.Date(releaseDates[1,1],"%m/%d/%Y"),"%Y")
+    ),
+  type = 'default',
+  text = "R is amazing.  Here is how it got that way.  Thanks to all the prolific contributors.
+  Thanks Ramnath Vaidyanathan for <a href ='http://rcharts.io/site)'>rCharts</a> especially
+  this <a href='http://ramnathv.github.io/sachin100s/'>timeline</a> which provided the code to generate this timeline in R.",
+  startDate =  format(as.Date(tail(releaseDates,1)[,1],"%m/%d/%Y"),"%Y,%m,%d"),
+  asset = list(media = 'http://r-project.org')
+)
+m$config(
+  font = "Merriweather-Newscycle"
+)
+names(releaseList) <- NULL
+m$event(releaseList)
+m$save('index.html')
+
+
+# Modify JS Path to use Local Assets
+x <- paste(readLines('index.html', warn = F), collapse = '\n')
+x <- gsub('C:/Program Files/R/R-3.0.1/library/rCharts/libraries/timeline', 'compiled', x)
+writeLines(x, con = 'index.html')
